1963582f4d6f75926974ac5178e1d8a8f5563d56
Jason Green <jgreen44@asu.edu>
2023-02-09 19:24:50 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
+import {UserProvider} from './contexts/user.context';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
