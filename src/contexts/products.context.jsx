76794ebc55ed33df0736e5a39e247ef0ea631d17
Jason Green <jgreen44@asu.edu>
2023-02-12 21:24:24 -0600

    relative link fix

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from 'react';
-import PRODUCTS from '../shop-data.json ';
+import PRODUCTS from '../shop-data.json';
 
 export const ProductsContext = createContext({
   products: [],
